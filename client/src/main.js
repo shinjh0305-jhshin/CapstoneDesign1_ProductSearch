b4b6b97a4a40b4d726c511191f55371a4a9e4b71
Jaehyun Shin_Linux <tlswogus30@naver.com>
2022-11-11 00:19:35 +0900

    ready to insall vuetify

--- client/src/main.js
+++ client/src/main.js
@@ -2,9 +2,8 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import router from "./router";
-import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-createApp(App).use(router).use(BootstrapVue).use(IconsPlugin).mount("#app");
+createApp(App).use(router).mount("#app");
--- client/vite.config.js
+++ client/vite.config.js
@@ -6,21 +6,10 @@ const target = "http://127.0.0.1:8080";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          compatConfig: {
-            MODE: 2,
-          },
-        },
-      },
-    }),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src/"),
-      vue: "@vue/compat",
     },
   },
 });
